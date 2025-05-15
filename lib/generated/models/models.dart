86c448fdfa9aa760c093a8d3f8d0b76cc2a91c7e
Albaraazain <albaraazain33@gmail.com>
2025-05-15 17:38:43 +0300

    generated files - not part of the package

--- lib/generated/models/models.dart
+++
@@ -1,6 +0,0 @@
-// Generated models barrel file
-
-export 'game_players_model.dart';
-export 'games_model.dart';
-export 'player_stats_model.dart';
-export 'users_model.dart';
--- lib/generated/repositories/repositories.dart
+++
@@ -1,8 +0,0 @@
-// Generated repositories barrel file
-
-export 'base_repository.dart';
-
-export 'game_players_repository.dart';
-export 'games_repository.dart';
-export 'player_stats_repository.dart';
-export 'users_repository.dart';
